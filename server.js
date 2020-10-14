958b8a9699154a5a3400311d983e831c0481a83e
moshe <moshemachluf97@gmail.com>
2020-10-14 15:34:35 +0300

    commit

--- server.js
+++ server.js
@@ -54,6 +54,7 @@ app.use("/api/users", usersRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/dist/client")));
+   
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client/dist/client", "index.html"));
   });
